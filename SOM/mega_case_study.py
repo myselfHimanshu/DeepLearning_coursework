9ce59cea325536649134897e936d3a323bb04617
myselfHimanshu <047himanshu@gmail.com>
2017-08-28 16:35:22 +0530

    update

--- SOM/mega_case_study.py
+++ SOM/mega_case_study.py
@@ -97,7 +97,7 @@ classifier.fit(customers, is_fraud, batch_size = 1, epochs = 2)
 
 # Predicting the probabilities of fraud
 y_pred = classifier.predict(customers)
-y_pred = np.concatenate((dataset.iloc[:,0:1].values,),axis=1)
+y_pred = np.concatenate((dataset.iloc[:,0:1].values,y_pred),axis=1)
 y_pred = y_pred[y_pred[:,1].argsort()]
 
 
